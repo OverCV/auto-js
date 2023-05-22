dfd0a689c2f0c1e0dc27d9ceff04ce209785c53d
vogel <over.castrillon22396@ucaldas.edu.co>
2023-05-21 21:29:50 -0500

    Now the automata can be created from a the complete table, it will require extra logic for it, but is functional. Also I notice that the metod it uses provides a way that taking into account the actual state where it begins, makes a complete  automata.

--- src/logic/determiner.js
+++ src/logic/determiner.js
@@ -29,15 +29,16 @@ export default class Determiner {
         let rowStates = [this.automata.getState('C'), this.automata.getState('H'), this.automata.getState('F')]
 
         let initialState = this.automata.getInitialState()
-        let table = this.setAdjacentTable([initialState])
-        console.log(table)
+        // let table = this.setAdjacentTable([initialState])
+        this.tableCreation()
+        // console.log(table)
 
         // Auto refactoring
         // let auto = this.automataFormat()
         // Auto assignment
         // return auto
     }
-
+    // !
     formatAutomata(adjacentTable) {
 
 
@@ -68,6 +69,7 @@ export default class Determiner {
         return new Automata()
     }
 
+    // !
     setAdjacentTable(states, visitedTable = []) {
         let statesKeys = states.map(state => {
             return state.getData()
@@ -99,6 +101,7 @@ export default class Determiner {
                 }
             })
             if (!visitedState) {
+                visitedTable.push(newRow)
                 console.log('not:v:', tCharStates)
                 let states = []
                 tCharStates.forEach(stateData => {
@@ -111,6 +114,88 @@ export default class Determiner {
             } else { return }
         }
         return visitedTable
+    }
+
+
+    tableCreation() {
+        let autoTable = []
+        let newRows = []
+
+        // Set the initial state, the ankle
+        let initialState = [this.automata.getInitialState()]
+        let row = this.newRowTable(initialState)
+
+        // Initialize the first rows
+        autoTable.push(row)
+        newRows = this.colToRow(autoTable)
+
+        // Table is initialized into the initial state as ankle for the next columns
+        // and the newRows list has information, that would be reassigned after every
+        // row of the full table has been reviewed (process of rowCreation).
+        while (newRows.length != 0) {
+            // Generate the new rows from the table actual states.
+            newRows = this.colToRow(autoTable)
+
+            // Previous rows are unique, should be added for the next iteration.
+            newRows.forEach(row => {
+                autoTable.push(row)
+            })
+        }
+        console.table(autoTable)
+        console.log(autoTable)
+    }
+
+    /**
+     * Given the columns, this function generates and push the new row into the table.
+     */
+    newRowTable(states) {
+        let statesData = states.map(state => {
+            return state.getData()
+        })
+        let row = {}
+        row['origin'] = statesData
+        row['destiny'] = this.mergeSubsets(states)
+        return row
+    }
+
+    /**
+     * If the table has one column that isn't evaluated, a new row must be created and
+     * returned into an array of values.
+     * Format: { origin: [states], destiny: { char: [states], ..., char: [states] } }.
+     */
+    colToRow(table) {
+        let newRows = []
+
+        // Select the list of destiny states
+        table.forEach(outRow => {
+            let exists = false
+            for (let tChar in outRow['destiny']) {
+                let destinies = outRow['destiny'][tChar]
+
+                table.forEach(inRow => {
+                    let origins = inRow['origin']
+
+                    // Compare the list with the origin states
+                    // console.log('origin', origins, '| destiny', destinies)
+
+                    let existingState = this.sameElements(origins, destinies)
+                    if (existingState) {
+                        exists = true
+                    }
+                })
+                // If one list of destiny states isn't on the origin, add as newRow.
+                if (!exists) {
+                    destinies = destinies.map(stateData => {
+                        return this.automata.getState(stateData)
+                    })
+                    // console.log('destiny', destinies)
+                    newRows.push(this.newRowTable(destinies))
+                }
+            }
+        })
+
+        // console.log('newRows', newRows)
+        return newRows
     }
 
     sameElements(list1, list2) {
